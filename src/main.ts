3c9b9e630f1179f1273d9e1783471ca77384e72a
Lukasz-Lipinski <l.lipinski1993@gmail.com>
2023-01-23 07:37:09 +0100

    custom colors setup

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
-import 'bootstrap/scss/bootstrap.scss';
 import {
   plugin,
   defaultConfig,
 } from '@formkit/vue';
 import '@formkit/themes/genesis';
+import 'bootstrap/scss/bootstrap.scss';
+import '@/styles/custom.scss';
 
 createApp(App)
   .use(plugin, defaultConfig)
